d807535904fe01d09509add47ab4e5219ccd4646
FREDOLIN B BRONE <110708518+Fredolin-B@users.noreply.github.com>
2023-09-21 16:27:38 +0530

    Add files via upload

---
+++ Day 7/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './css/App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Login/>}/>
+      <Route exact path='/register' element={<Register/>}/>
+      <Route exact path='/home' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+export default App;
